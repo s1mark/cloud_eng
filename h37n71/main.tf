ac230e8d5532c948fa7dd8ea466fabae24f3bce5
waleedMuslem <waleedmosllm2020@gmail.com>
2023-03-16 14:41:07 +0100

    created Hello world

--- h37n71/main.tf
+++ h37n71/main.tf
@@ -1,5 +1,11 @@
-resource "null_resource" "hello_world" {
-  provisioner "local-exec" {
-    command = "echo 'Hello, world!'"
-  }
+resource "local_file" "local" {
+  content  = "Hello world"
+  filename = "${path.module}/local.bar"
+}
+
+
+resource "local_file" "example" {
+  count    = 10
+  content  = "This is the content of file ${count.index+1}."
+  filename = "/path/to/file${count.index}.txt"
 }
