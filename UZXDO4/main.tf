db6ba1591e46496e7f196676630f6ef7b6c301c6
RachidAifar <90982692+RachidAifar@users.noreply.github.com>
2023-03-16 14:30:34 +0100

    feat<UZXDO4>:create 10 local terraform file

--- UZXDO4/main.tf
+++ UZXDO4/main.tf
@@ -2,3 +2,10 @@ resource "local_file" "local" {
   content  = "Hello world"
   filename = "${path.module}/local.bar"
 }
+
+
+resource "local_file" "example" {
+  count    = 10
+  content  = "This is the content of file ${count.index+1}."
+  filename = "/path/to/file${count.index+1}.txt"
+}
