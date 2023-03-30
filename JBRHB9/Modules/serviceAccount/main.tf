efb5c53d8f8d67772cb91dbda644540a02119f06
Ahmed <ahmelsamahy@gmail.com>
2023-03-30 13:58:58 +0200

    feat: service account with my neptun code

---
+++ JBRHB9/Modules/serviceAccount/main.tf
@@ -0,0 +1,7 @@
+module "service_accounts" {
+  source        = "terraform-google-modules/service-accounts/google"
+  version       = "~> 1.0"
+  project_id    = "ekke-cloud"
+  names         = ["<JBRHB9>"]
+  project_roles = ["roles/viewer"]
+}
