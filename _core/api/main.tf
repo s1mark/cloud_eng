0b55d541161b7e8e9a11f3b7b267fe8539141794
Yslamguly <islamguly28@gmail.com>
2023-03-30 14:48:46 +0200

    reformated solution

--- _core/api/main.tf
+++ _core/api/main.tf
@@ -1,11 +1,6 @@
 module "project-services" {
   source  = "terraform-google-modules/project-factory/google//modules/project_services"
   version = "~> 14.2"
-
-  project_id                  = "ekke-cloud"
-
-  activate_apis = [
-    "compute.googleapis.com",
-    "iam.googleapis.com",
-  ]
+  project_id = "ekke-cloud"
+  activate_apis = ["compute.googleapis.com"]
 }
