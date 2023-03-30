03e143cf3a2753c26cc2569299afef6ebbec1839
Mark Simon <simark2357@gmail.com>
2023-03-30 15:24:24 +0200

    made it work

--- _core/compute_instance/Main.tf
+++ _core/compute_instance/Main.tf
@@ -1,5 +1,14 @@
-module "compute_instance" {
-  source              = "terraform-google-modules/terraform-google-vm/google//modules/compute_instance"
-  instance_template   = module.instance_template.self_link
+module "api" {
+  source = "../api"
+}
 
+module "instance_template" {
+  source = "../instance_template"
+  depends_on = [module.api]
+}
+
+module "compute_instance" {
+  source              = "git::https://github.com/terraform-google-modules/terraform-google-vm//modules/compute_instance"
+  instance_template   = module.instance_template.self_link
+  zone = "us-west1-a"
 }
--- _core/instance_template/main.tf
+++ _core/instance_template/main.tf
@@ -4,12 +4,22 @@ variable "service_account" {
     email  = string
     scopes = set(string)
   })
+  default = {
+    email = "92227481840-compute@developer.gserviceaccount.com"
+    scopes = ["cloud-platform"]
+  }
 }
 
 module "instance_template" {
-  source = "git::https://github.com/terraform-google-modules/terraform-google-vm/modules/instance_template"
+  source = "git::https://github.com/terraform-google-modules/terraform-google-vm//modules/instance_template"
   project_id    = "ekke-cloud"
   disk_size_gb = "20"
   machine_type = "f1-micro"
   service_account              = var.service_account
+  network = "default"
+
+}
+
+output "self_link" {
+  value = module.instance_template.self_link
 }
