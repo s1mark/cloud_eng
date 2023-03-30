240111b224edf7386a636f8b408d9b506d79689c
Ahmed <ahmelsamahy@gmail.com>
2023-03-30 14:03:19 +0200

    refactor: change var "names" and project_roles

--- JBRHB9/Modules/serviceAccount/main.tf
+++ JBRHB9/Modules/serviceAccount/main.tf
@@ -2,6 +2,6 @@ module "service_accounts" {
   source        = "terraform-google-modules/service-accounts/google"
   version       = "~> 1.0"
   project_id    = "ekke-cloud"
-  names         = ["<JBRHB9>"]
-  project_roles = ["roles/viewer"]
+  names         = ["JBRHB9"]
+  project_roles = ["project_id=>roles/viewer"]
 }
