8d1498bc278f3f42ab07389eef4370a138466948
Mark Simon <33624175+s1mark@users.noreply.github.com>
2023-05-04 14:11:52 +0200

    Update mzl7y1.tf

--- mzl7y1.tf
+++ mzl7y1.tf
@@ -1,3 +1,14 @@
 module "mzl7y1-pipeline" {
   source = "./mzl7y1"
 }
+  
+provider "google" {
+  project = "ekke-cloud"
+}
+
+data "google_project" "project" {
+}
+
+output "project_number" {
+  value = data.google_project.project.number
+}
