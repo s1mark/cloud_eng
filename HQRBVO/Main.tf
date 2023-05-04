97792f86285710ef3b807a4451e8717081a9173e
Dany Razzoqa <91144866+danirazzouqa@users.noreply.github.com>
2023-05-04 14:31:16 +0200

    Update Main.tf

--- HQRBVO/Main.tf
+++ HQRBVO/Main.tf
@@ -1,5 +1,13 @@
-resource "local_file" "file" {
-  count = 5
-  content  = var.file
-  filename = "C:\\Users\\user\\terraform\\${count.index}.txt" 
+
+
+resource "local_file" "local" {
+  content  = var.file_content
+  filename = "${path.module}/local.bar"
+}
+
+
+resource "local_file" "example" {
+  count    = 10
+  content  = "This is the content of file ${count.index+1}."
+  filename = "./${count.index+1}.txt"
 }
